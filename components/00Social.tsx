e78ceb34f61112e8ae9dc599782db3ec905beffe
Mikha38 <oyks1379@yandex.ru>
2024-04-29 22:59:42 +0300

    Правки

--- components/00Social.tsx
+++ components/00Social.tsx
@@ -7,7 +7,7 @@ import x_reverse from '../public/img/00Social/x_reverse.svg'
 export default function Social({ reverse, className }: { reverse?: boolean, className?: string }){
     return(
         <div className={"flex gap-x-1 relative z-30" + " " + className}>
-            <a href='https://twitter.com/Alfadog_SOL'>
+            <a href='https://twitter.com/SOL_ALFADOG'>
                 <Image src={ reverse ? x_reverse : x} alt="Twitter" className="block w-[30px] xs:w-auto"/>
             </a>
             <a href='https://t.me/alfa_dog_sol'>
